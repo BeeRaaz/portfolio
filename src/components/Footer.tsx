a63ce63d00e4064abccd3a2137699732d3171c08
BeeRaaz <137605748+BeeRaaz@users.noreply.github.com>
2025-01-23 11:38:36 +0545

    small fix

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,28 +4,32 @@ import { Button } from "./ui/button";
 import { Github, Linkedin } from "lucide-react";
 
 export default function Footer() {
-
-    const socialLinks = [
-        {
-            path: 'https://github.com/BeeRaaz',
-            label: 'GitHub',
-            icon: <Github />
-        },
-        {
-            path: 'https://linkedin.com/in/birajdulal',
-            label: 'LinkedIn',
-            icon: <Linkedin />
-        },
-    ]
+  const socialLinks = [
+    {
+      path: "https://github.com/BeeRaaz",
+      label: "GitHub",
+      icon: <Github />,
+    },
+    {
+      path: "https://linkedin.com/in/birajdulal",
+      label: "LinkedIn",
+      icon: <Linkedin />,
+    },
+  ];
 
   return (
     <footer className="py-5 border border-t-border">
       <Container>
         <ul className="flex flex-wrap justify-center items-center gap-3">
-            {socialLinks && socialLinks.map(social => (
+          {socialLinks &&
+            socialLinks.map((social) => (
+              <li key={social.label}>
                 <Link href={social.path} aria-label={social.label}>
-                    <Button variant={'outline'} size={'icon'} >{social.icon}</Button>
+                  <Button variant={"outline"} size={"icon"}>
+                    {social.icon}
+                  </Button>
                 </Link>
+              </li>
             ))}
         </ul>
       </Container>
